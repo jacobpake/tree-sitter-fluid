299ca04a90bb49eff7aa7ff6424cc616c457b59b
Jacob Pake <github@pake.dev>
2025-11-23 13:30:55 +0000

    scaffolding

--- grammar.js
+++ grammar.js
@@ -6,12 +6,87 @@
 
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
+//
+//
+const TODO = ($) => "TODO";
 
 module.exports = grammar({
   name: "fluid",
 
   rules: {
-    // TODO: add the actual grammar rules
     source_file: ($) => "hello",
+
+    var_def: TODO,
+    rec_def: TODO,
+
+    _block: ($) => choice($.match_as, $.if_else, $.def, $._expr),
+
+    match_as: TODO,
+    if_else: TODO,
+    def: TODO,
+
+    _expr: ($) => choice($.binary_app, $.project, $.dproject, $.app, $._simple),
+
+    binary_app: TODO,
+    project: TODO,
+    dproject: TODO,
+    app: TODO,
+
+    _simple: ($) =>
+      choice(
+        $.matrix,
+        $.list_empty,
+        $.list_non_empty,
+        $.list_enum,
+        $.lambda,
+        $.dict,
+        $.paragraph,
+        $.doc,
+        $.pair,
+        $._parens_op,
+        $._parens_expr,
+      ),
+
+    matrix: TODO,
+    list_empty: TODO,
+    list_non_empty: TODO,
+    list_enum: TODO,
+    list_comp: TODO,
+    lambda: TODO,
+    dict: TODO,
+    paragraph: TODO,
+    doc: TODO,
+    pair: TODO,
+    _parens_op: TODO,
+    _parens_expr: TODO,
+
+    _pattern: ($) => choice($.pattern_binary_app, $.pattern_simple),
+
+    pattern_binary_app: TODO,
+
+    _pattern_simple: ($) =>
+      choice(
+        $.pattern_var,
+        $.pattern_constr,
+        $.pattern_record,
+        $.pattern_list,
+        $.pattern_pair,
+        $._pattern_parens,
+      ),
+
+    pattern_var: TODO,
+    pattern_constr: TODO,
+    pattern_record: TODO,
+    pattern_list: TODO,
+    pattern_pair: TODO,
+
+    _pattern_parens: TODO,
+
+    var: (_) => token(/[a-z_][a-zA-Z0-9_]*/),
+    constr: (_) => token(/[A-Z][a-zA-Z0-9_]*/),
+    operator: (_) => token(/[:!#$%&*+./<=>?@\\^|~-]+/),
+    float: (_) => token(/[+-]?[0-9]+\.[0-9]+/),
+    integer: (_) => token(/[+-]?[0-9]+/),
+    str: (_) => token(/"[^"\\]*(?:\\.[^"\\]*)*"/),
   },
 });
